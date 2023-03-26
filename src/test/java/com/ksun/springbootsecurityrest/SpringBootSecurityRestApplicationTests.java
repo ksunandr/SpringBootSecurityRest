7b5ca2b6bf00ac5cf22fa25fee46f211791b76b3
kseniia.andreeva <ksun.andr@gmail.com>
2023-03-26 12:14:27 +0100

    initial

---
+++ src/test/java/com/ksun/springbootsecurityrest/SpringBootSecurityRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksun.springbootsecurityrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
